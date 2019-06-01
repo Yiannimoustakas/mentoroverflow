4251431e3c120db4cb3c2ca7e83c89959269b674
Rejaine Lima <relfarias@gmail.com>
2019-06-01 15:26:25 +1000

    Home screen
    Add List

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,40 +3,44 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/widgets.dart';
-
 import 'list.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return CupertinoTabScaffold(
-      tabBar: CupertinoTabBar(items: [
-        BottomNavigationBarItem(
-          icon: Icon(CupertinoIcons.home),
-          title: Text('Home'),
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Mentot Overflow"),
         ),
-        BottomNavigationBarItem(
-          icon: Icon(CupertinoIcons.book),
-          title: Text('Mentor'),
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(CupertinoIcons.search),
-          title: Text('Search'),
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(CupertinoIcons.settings),
-          title: Text('Settings'),
-        ),
-      ]),
-      tabBuilder: (context, index) {
-        if (index == 0) {
-          return ListScreen();
-        }  else {
-          return MentorScreen();
-        }
-      },
+        body: ListScreen()
     );
+
+//    return CupertinoTabScaffold(
+//      tabBar: CupertinoTabBar(items: [
+//        BottomNavigationBarItem(
+//          icon: Icon(CupertinoIcons.home),
+//          title: Text('Home'),
+//        ),
+//        BottomNavigationBarItem(
+//          icon: Icon(CupertinoIcons.book),
+//          title: Text('Mentor'),
+//        ),
+//        BottomNavigationBarItem(
+//          icon: Icon(CupertinoIcons.search),
+//          title: Text('Search'),
+//        ),
+//        BottomNavigationBarItem(
+//          icon: Icon(CupertinoIcons.settings),
+//          title: Text('Settings'),
+//        ),
+//      ]),
+//      tabBuilder: (context, index) {
+//        if (index == 0) {
+//          return ListScreen();
+//        }  else {
+//          return MentorScreen();
+//        }
+//      },
+//    );
   }
 }
