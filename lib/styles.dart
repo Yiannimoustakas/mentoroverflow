feba96b7c7bb7bc9a7e63a86574adf74062ca196
p4r4n01d <p4r4n01d42@gmail.com>
2019-06-01 14:13:15 +1000

    Initial profile page

---
+++ lib/styles.dart
@@ -0,0 +1,25 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  
+  static const labelText = TextStyle(
+    color: Color.fromRGBO(176, 176, 176, 1.0),
+    fontFamily: 'NotoSans',
+    fontSize: 20.0,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.bold,
+  );
+  
+  static const chipText = TextStyle(
+    fontSize: 18.0,
+  );
+  
+  static const labelValue = TextStyle(
+    fontSize: 18.0,
+  );
+
+}
