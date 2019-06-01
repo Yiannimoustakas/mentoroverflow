744688b3f55b4a1b6d30d40f18f8dc674d5a27fb
Bennie Botha <bennie@moroku.com>
2019-06-01 11:57:49 +1000

    Added basic chat View

---
+++ lib/widgets/chat_screen/chat_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:mentoroverflow/widgets/chat_screen/chat_screen.dart';
+
+class ChatPage extends StatelessWidget {
+  const ChatPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Chat App"),
+      ),
+      body: SafeArea(
+        child: ChatScreen()
+      )
+    );
+  }
+}
